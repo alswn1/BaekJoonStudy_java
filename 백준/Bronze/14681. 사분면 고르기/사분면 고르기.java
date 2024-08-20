632124f360ac370d342dadaaa6aa4a062b8e3e57
Jade Bead <91466490+alswn1@users.noreply.github.com>
2024-08-20 15:16:37 +0900

    [Bronze V] Title: 사분면 고르기, Time: 104 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+        if (a > 0) {
+            if (b > 0) System.out.println(1);
+            else if (b < 0) System.out.println(4);
+        }else if (a < 0) {
+            if (b > 0) System.out.println(2);
+            else if (b < 0) System.out.println(3);
+        }
+    }
+}
