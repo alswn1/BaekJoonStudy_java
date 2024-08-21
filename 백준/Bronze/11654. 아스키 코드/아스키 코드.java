b7bf42f2670570b56ac34151f6ef0ce4799600f0
Jade Bead <91466490+alswn1@users.noreply.github.com>
2024-08-21 10:39:41 +0900

    [Bronze V] Title: 아스키 코드, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = br.readLine().charAt(0);
+        System.out.println(a);
+    }
+}
