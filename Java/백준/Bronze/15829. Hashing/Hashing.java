a41ab8a36d6b0156cd6a71ad695861abb57e8d87
Jade Bead <91466490+alswn1@users.noreply.github.com>
2024-08-27 09:44:22 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14224 KB, Score: 100 point -BaekjoonHub

--- Java/백준/Bronze/15829. Hashing/Hashing.java
+++ Java/백준/Bronze/15829. Hashing/Hashing.java
@@ -7,10 +7,13 @@ public class Main {
         int num = Integer.parseInt(br.readLine());
         String str = br.readLine();
         long answer = 0;
+        int a = 1234567891;
+        long pow = 1;
 
         for (int i = 0; i < num; i++) {
             char n = str.charAt(i);
-            answer += ((int)n-96) * Math.pow(31, i);
+            answer = (answer + ((n-'a'+1) * pow) % a) % a;
+            pow = (pow * 31) % a;
         }
 
         System.out.println(answer);
