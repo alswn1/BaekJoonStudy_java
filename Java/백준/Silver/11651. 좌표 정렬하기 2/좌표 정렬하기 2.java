f7055f19465734a78d8c8e0c060e7188ebeec563
Jade Bead <91466490+alswn1@users.noreply.github.com>
2024-08-27 13:35:33 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 632 ms, Memory: 52968 KB -BaekjoonHub

--- Java/백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
+++ Java/백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -4,7 +4,6 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
         int num = Integer.parseInt(br.readLine());
         int[][] arr = new int[num][2];
@@ -23,11 +22,12 @@ public class Main {
             }
         });
 
+        StringBuilder sb = new StringBuilder();
+
         for (int[] is : arr) {
-            bw.write(is[0] + " " + is[1] + "\n");
+            sb.append(is[0]).append(" ").append(is[1]).append("\n");
         }
 
-        bw.flush();
-        bw.close();
+        System.out.println(sb.toString().trim());
     }
 }
