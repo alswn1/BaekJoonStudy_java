29466455b78d18792eb46911e123ed44871640ef
Jade Bead <91466490+alswn1@users.noreply.github.com>
2024-08-28 09:41:14 +0900

    [Silver IV] Title: 카드2, Time: 168 ms, Memory: 53352 KB -BaekjoonHub

---
+++ Java/백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= N; i++) {
+            queue.add(i);
+        }
+
+        while (queue.size() > 1) {
+            queue.poll();
+            int a = queue.poll();
+            queue.add(a);
+        }
+
+        System.out.println(queue.peek());
+    }
+}
